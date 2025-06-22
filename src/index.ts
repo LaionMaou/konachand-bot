c9de01ef16bc7246c11f3ed4fe6198f442cc5e0a
LaionMaou <140976635+LaionMaou@users.noreply.github.com>
2025-06-22 15:53:43 -0300

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+import { initWebSocket, registerIncomingWSHandler } from './ws';
+import dotenv from 'dotenv';
+import { handleMessage } from './events/message';
+
+dotenv.config();
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+});
+
+client.on('ready', () => {
+  console.log(`✅ Conectado como ${client.user?.tag}`);
+  initWebSocket(client);
+});
+
+client.on('messageCreate', async (msg) => {
+  if (!msg.author.bot) {
+    handleMessage(msg);
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
